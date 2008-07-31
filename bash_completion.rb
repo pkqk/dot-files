9c100baa85bf42380dccd38456b548dd8d79aa3a
adam <adam@pkqk.net>
2008-07-31 12:23:39 +1000

    cap tasks completion

--- bash_completion.rb
+++ bash_completion.rb
@@ -1,31 +1,55 @@
 #!/usr/bin/env ruby
 
-# Save this somewhere, chmod 755 it, then add
-#   complete -C path/to/this/script -o default rake
-# to your ~/.bashrc
-#
 # If you update your tasks, just $ rm ~/.raketabs*
 #
 # Adapted from 
+# http://errtheblog.com/posts/31-rake-around-the-rosie
+# Adapted from 
 # http://onrails.org/articles/2006/08/30/namespaces-and-rake-command-completion
 
+def rakefile
+  File.join(Dir.pwd, 'Rakefile')
+end
 
-def rake_silent_tasks
+def rake_tasks
   if File.exists?(dotcache = File.join(File.expand_path('~'), ".raketabs-#{Dir.pwd.hash}"))
-    File.read(dotcache)
-  else
-    tasks = `rake --silent --tasks`
-    File.open(dotcache, 'w') { |f| f.puts tasks }
-    tasks
+    return File.read(dotcache) if File.mtime(rakefile) < File.mtime(dotcache)
   end
+  tasks = `rake --silent --tasks`
+  File.open(dotcache, 'w') { |f| f.puts tasks }
+  tasks
+end
+
+def cap_tasks
+  if File.exists?(dotcache = File.join(File.expand_path('~'), ".captabs-#{Dir.pwd.hash}"))
+    return File.read(dotcache)
+  end
+  tasks = `cap -Tv | grep ^cap | cut -d' ' -f2`
+  File.open(dotcache, 'w') { |f| f.puts tasks }
+  tasks
 end
 
 def complete_rake_tasks
-  return [] unless File.file?(File.join(Dir.pwd, 'Rakefile'))
-  return [] unless /^rake\b/ =~ ENV["COMP_LINE"]
+  return [] unless File.file?(rakefile)
+  return [] unless /\brake\b/ =~ ENV["COMP_LINE"]
   after_match = $'
   task_match = (after_match.empty? || after_match =~ /\s$/) ? nil : after_match.split.last
-  tasks = rake_silent_tasks.split("\n")[1..-1].map { |line| line.split[1] }
+  tasks = rake_tasks.split("\n")[1..-1].map { |line| line.split[1] }
+  tasks = tasks.select { |t| /^#{Regexp.escape task_match}/ =~ t } if task_match
+  # handle namespaces
+  if task_match =~ /^([-\w:]+:)/
+    upto_last_colon = $1
+    after_match = $'
+    tasks = tasks.map { |t| (t =~ /^#{Regexp.escape upto_last_colon}([-\w:]+)$/) ? "#{$1}" : t }
+  end
+  tasks
+end
+
+def complete_cap_tasks
+  return [] unless /\bcap\b/ =~ ENV["COMP_LINE"]
+  after_match = $'
+  task_match = (after_match.empty? || after_match =~ /\s$/) ? nil : after_match.split.last
+  tasks = cap_tasks.split("\n")
   tasks = tasks.select { |t| /^#{Regexp.escape task_match}/ =~ t } if task_match
   # handle namespaces
   if task_match =~ /^([-\w:]+:)/
