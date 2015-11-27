021e7f81aa5477f9627f0401f458c116619f6859
Adam Sven Johnson <adam@pkqk.net>
2015-11-27 11:16:12 +0000

    Link bundle folder

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 git submodule update --init
 path=`pwd`
 pushd ~
-for i in bashrc inputrc screenrc vim vimrc gemrc gitconfig gitignore profile
+for i in bashrc bundle inputrc screenrc vim vimrc gemrc gitconfig gitignore profile
 do
   ln -sf "${path##$HOME/}/${i}" ~/.$i
 done
