7bc071d63b6293130f553026902fc6fd7d120aa5
Adam Sven Johnson <adam@pkqk.net>
2022-01-21 12:00:11 +1300

    Use sorted list for setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,21 @@
 git submodule update --init
 path=$(pwd)
 pushd ~ > /dev/null
-for i in bashrc inputrc screenrc vim vimrc gemrc gitconfig gitignore profile digrc
+
+files=(
+  bashrc
+  digrc
+  gemrc
+  gitconfig
+  gitignore
+  inputrc
+  profile
+  screenrc
+  vim
+  vimrc
+)
+
+for i in "${files[@]}"
 do
   echo "linking ~/${path##$HOME/}/${i} to ~/.$i"
   ln -shf "${path##$HOME/}/${i}" ~/.$i
