25fbf5cce3099ace42b69b7d7ab0b0f28d5bab43
Adam Sven Johnson <adam@pkqk.net>
2022-01-21 11:55:46 +1300

    Improve setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,9 @@
 git submodule update --init
-path=`pwd`
-pushd ~
+path=$(pwd)
+pushd ~ > /dev/null
 for i in bashrc inputrc screenrc vim vimrc gemrc gitconfig gitignore profile digrc
 do
+  echo "linking ~/${path##$HOME/}/${i} to ~/.$i"
   ln -shf "${path##$HOME/}/${i}" ~/.$i
 done
-popd
+popd > /dev/null
