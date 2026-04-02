95b9d84186e7ffab2af215f69172ba71cbe8ba8a
Adam Sven Johnson <adam@pkqk.net>
2026-04-02 15:23:52 +1300

    Update path config

--- .profile.d/00-paths.sh
+++ .profile.d/00-paths.sh
@@ -1,9 +1,5 @@
 paths=(
   ~/bin
-  ~/go/bin
-  ~/go/bin/darwin_arm64
+  /usr/local/go/bin
   /opt/homebrew/bin
-  /opt/homebrew/opt/mysql-client@5.7/bin
-  /opt/homebrew/opt/node@20/bin
-  /opt/homebrew/opt/libpq/bin
 )
--- .profile.d/50-java.sh
+++ .profile.d/50-java.sh
@@ -1,4 +1,7 @@
 # OpenJDK@11 homebrew
-paths+=(/opt/homebrew/opt/openjdk@11/bin)
-export JAVA_HOME=/opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk/Contents/Home/
-export CPPFLAGS="-I/opt/homebrew/opt/openjdk@11/include"
+if command -v /opt/homebrew/opt/openjdk@11/bin/java >/dev/null 2>&1
+then
+  paths+=(/opt/homebrew/opt/openjdk@11/bin)
+  export JAVA_HOME=/opt/homebrew/opt/openjdk@11/libexec/openjdk.jdk/Contents/Home/
+  export CPPFLAGS="-I/opt/homebrew/opt/openjdk@11/include"
+fi
